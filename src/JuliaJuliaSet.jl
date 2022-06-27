592adc29082cc000792223aeaea1c8eacf07be10
KauBoui <errolbucy@gmail.com>
2022-06-27 17:53:37 -0600

    Parameterized Paths and Julia Set Progressions!

--- src/JuliaJuliaSet.jl
+++ src/JuliaJuliaSet.jl
@@ -1,8 +1,9 @@
 module JuliaJuliaSet
 
 using LinearAlgebra
-using Distributed
-using Plots
+using Plots, Colors
+
+# ----- Functions for generating Julia and Mandelbrot sets -----
 
 function julia(f::Function, c::Complex{Float64}, z::Complex{Float64}, threshold::Float64)
     for i = 1:255
@@ -25,6 +26,12 @@ function mandelbrot(f::Function, c::Complex{Float64}, threshold::Float64)
     return -0
 end
 
+function setdims(max_coord, min_coord, resolution)
+    dim = (max_coord - min_coord) * resolution
+    @assert dim != 0
+    return dim
+end
+
 struct SetParams
     min_coord::Complex{Float64}
     max_coord::Complex{Float64}
@@ -37,8 +44,8 @@ struct SetParams
         return new(min_coord, 
             max_coord, 
             resolution, 
-            (max_coord.re - min_coord.re) * resolution ; @assert width != 0,
-            (max_coord.im - min_coord.im) * resolution ; @assert height != 0, 
+            setdims(max_coord.re, min_coord.re, resolution), 
+            setdims(max_coord.im, min_coord.im, resolution), 
             threshold,
             nr_frames) 
     end
@@ -57,12 +64,36 @@ function genplane(set_p::SetParams)
     return reverse!(complexplane, dims=1)
 end
 
-function genmandelbrotset(set_p::SetParams, f::Function, plane::Matrix{Complex64})
+function genmandelbrotset(set_p::SetParams, f::Function, plane::Matrix{ComplexF64})
     return mandelbrot.(f, plane, set_p.threshold)
 end
 
-function genjuliaset(set_p::SetParams, c, f::Function, plane::Matrix{Complex64})
-    return julia.(f, c, plane, set_p)
+function genjuliaset(set_p::SetParams, c, f::Function, plane::Matrix{ComplexF64})
+    return julia.(f, c, plane, set_p.threshold)
 end
 
+function genjuliaprogression(set_p::SetParams, P::Path, f::Function, plane::Matrix{ComplexF64})
+    c_vec = pointsoncruve(P,sep_p.nr_frames)
+    return [genjuliaset(set_p, c, f, plane) for c ∈ c_vec]
+end
 
+# ----- Functionality for defining paths through the complex plane -----
+
+struct Path
+    parameterization::Function
+    start::ComplexF64
+    ending::ComplexF64
+end
+
+Circle_Path(r::Real)::Path = Path(t-> (r*ℯ^(t*1im)),0,2π)
+
+pointsoncurve(P::Path, n::Integer) = [P.parameterization(t) for t in range(P.start, P.ending,length=n)]
+
+# ----- Functions for animating series of sets -----
+
+function animateprogression(sets::Vector{Matrix{Int64}}, set_p::SetParams, file_name::String ="~/GIFs/julia_$(p.min_coord)_$(p.max_coord)_$(p.height)_$(p.width)_$(p.nr_frames).gif")
+    anim = @animate for set ∈ sets
+        heatmap(set, size=(set_p.width,set_p.height), color=:terrain, leg=false)
+    end
+    gif(anim, file_name, fps=30)
+end
